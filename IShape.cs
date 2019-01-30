56b96bdcf1e2c7acd2987b4735eaa65661a316e9
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-01-30 21:33:37 +0200

    Interfaces for Shape and Texture

---
+++ IShape.cs
@@ -0,0 +1,13 @@
+﻿namespace PTSharp
+{
+    interface IShape
+    {
+        void Compile();
+        Box GetBoundingBox();
+        Hit Intersect(Ray ray);
+        Vector UV(Vector uv);
+        Vector NormalAt(Vector normal);
+        Material MaterialAt(Vector v);
+        //Box BoundingBox();
+    }
+}
---
+++ ITexture.cs
@@ -0,0 +1,12 @@
+﻿namespace PTSharp
+{
+    interface ITexture
+    {
+        Color Sample(double u, double v);
+        Vector NormalSample(double u, double v);
+        Vector BumpSample(double u, double v);
+        ITexture Pow(double a);
+        ITexture MulScalar(double a);
+    }
+}
+
