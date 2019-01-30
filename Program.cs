99b8395330fcf9072390ab21a04305c47829afe9
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-01-30 22:49:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,45 +10,22 @@ namespace PTSharp
     {
         static void Main(string[] args)
         {
-            int width = 640;
-            int height = 480;
-
+            int width = 1920;
+            int height = 1080;
             Scene scene = new Scene();
-
-            // eye, center, up
-            Camera camera = Camera.LookAt(new Vector(0, 50, 1), new Vector(4, 20, 5), new Vector(0, 0, 1), 50);
-
-            // Light
-            scene.Add(Sphere.NewSphere(new Vector(0, 0, 4), 0.5, Material.LightMaterial(Color.White, 20)));
-
-            // Plane
-            Plane plane = Plane.NewPlane(new Vector(0, 0, 0), new Vector(0, 0, 1), Material.SpecularMaterial(new Color(1.0, 1.0, 1.0), 1));
-            scene.Add(plane);
-
-            // Sphere
-            Material mat1 = Material.DiffuseMaterial(new Color(1.0, 1.0, 1.0));
-            Material mat2 = Material.GlossyMaterial(new Color(1.0, 0.0, 1.0), 1.4, Util.Radians(30));
-            Material mat3 = Material.MetallicMaterial(new Color(1.0, 1.0, 0.0), 1.5, 1.0);
-            Material mat4 = Material.SpecularMaterial(new Color(0.0, 1.0, 1.0), 1.0);
-            Material mat5 = Material.TransparentMaterial(new Color(1.0, 1.0, 1.0), 2, Util.Radians(20), 1);
-            Material mat6 = Material.ClearMaterial(0.5, 0.5);
-            Material lightmaterial = Material.LightMaterial(Color.White, 20);
-
-            // Spheres
-            scene.Add(Sphere.NewSphere(new Vector(-6, 0, 1), 1, mat1)); // Diffuse
-            scene.Add(Sphere.NewSphere(new Vector(-4, 0, 1), 1, mat2)); // Glossy
-            scene.Add(Sphere.NewSphere(new Vector(-2, 0, 1), 1, mat3)); // Metallic
-            scene.Add(Sphere.NewSphere(new Vector(0, 0, 1), 1, mat4));  // Specular
-            scene.Add(Sphere.NewSphere(new Vector(2, 0, 1), 1, mat5));  // Transparent
-            scene.Add(Sphere.NewSphere(new Vector(4, 0, 1), 1, mat6));  // Clear
-            
-            Sampler sampler = DefaultSampler.NewSampler(4, 4);
+            scene.Add(Sphere.NewSphere(new Vector(1.5, 1.25, 0), 1.25, Material.SpecularMaterial(Color.HexColor(0x004358), 1.3)));
+            scene.Add(Sphere.NewSphere(new Vector(-1, 1, 2), 1, Material.SpecularMaterial(Color.HexColor(0xFFE11A), 1.3)));
+            scene.Add(Sphere.NewSphere(new Vector(-2.5, 0.75, 0), 0.75, Material.SpecularMaterial(Color.HexColor(0xFD7400), 1.3)));
+            scene.Add(Sphere.NewSphere(new Vector(-0.75, 0.5, -1), 0.5, Material.ClearMaterial(1.5, 0)));
+            scene.Add(Cube.NewCube(new Vector(-10, -1, -10), new Vector(10, 0, 10), Material.GlossyMaterial(Color.White, 1.1, Util.Radians(10))));
+            scene.Add(Sphere.NewSphere(new Vector(-1.5, 4, 0), 0.5, Material.LightMaterial(Color.White, 30)));
+            Camera camera = Camera.LookAt(new Vector(0, 2, -5), new Vector(0, 0.25, 3), new Vector(0, 1, 0), 45);
+            camera.SetFocus(new Vector(-0.75, 1, -1), 0.1);
+            Sampler sampler = DefaultSampler.NewSampler(4, 8);
             Renderer renderer = Renderer.NewRenderer(scene, camera, sampler, width, height);
-
             renderer.AdaptiveSamples = 50;
             renderer.FireflySamples = 32;
-
-            renderer.IterativeRender("./out.png", 2);
+            renderer.IterativeRender("./out.png", 2);           
         }
     }
 }
