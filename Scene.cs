b193b6fedd6ff5c8a4ceef93e82c979bdb927618
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-01-30 21:55:58 +0200

    Update Scene.cs

--- Scene.cs
+++ Scene.cs
@@ -1,77 +1,76 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PTSharp
 {
     public class Scene
     {
-        public Color Color;
-        public Texture Texture;
-        public double TextureAngle;
-        public List<Shape> ShapeList;
-        public Shape[] Lights;
-        public Shape[] Shapes;
-        public Tree Tree;
-        public int rays;
-        public int raycount;
-
-        public Scene(Color color, Texture texture, double textureAngle, Shape[] shapes, Shape[] lights, Tree tree, int rays) {
-            this.Color = color;
-            this.Texture = texture;
-            this.TextureAngle = textureAngle;
-            this.Shapes = shapes;
-            this.Lights = lights;
-            this.Tree = tree;
-            this.rays = rays;
+        internal Color color;
+        internal ITexture Texture;
+        internal double textureAngle;
+        private List<IShape> shapeList;
+        private List<IShape> lightList;
+        internal IShape[] lights;
+        private IShape[] shapes;
+        private Tree tree;
+        internal int rays;
+        
+        Scene(Color color_, ITexture texture_, double textureAngle_, IShape[] shapes_, IShape[] lights_, Tree tree_, int rays_) {
+            color =color_;
+            Texture = texture_;
+            textureAngle = textureAngle_;
+            shapes = shapes_;
+            lights = lights_;
+            tree = tree_;
+            rays = rays_;
         }
 
-        public Scene() {
-            this.Color = new Color();
-            this.Shapes = new Shape[] { };
-            this.Lights = new Shape[] { };
-            ShapeList = new List<Shape>();
-            this.Texture = null;
+        public Scene()
+        {
+            color = new Color();
+            shapes = new IShape[] { };
+            lights = new IShape[] { };
+            shapeList = new List<IShape>();
+            lightList = new List<IShape>();
+            Texture = null;
         }
         
-        public void Compile() {
-            foreach(Shape shape in Shapes) {
+        public void Compile()
+        {
+            foreach (IShape shape in shapes)
+            {
                 shape.Compile();
             }
-            if (this.Tree == null) {
-                this.Tree = Tree.NewTree(Shapes);
+            if (tree == null)
+            {
+                tree = Tree.NewTree(shapes);
             }
         }
-        
-        public void Add(Shape shape)
-        {
-            ShapeList.Add(shape);
-            this.Shapes = ShapeList.ToArray();
 
-            // Small check to see if material is null
-            if(shape.MaterialAt(new Vector()) == null)
+        internal void Add(IShape shape)
+        {
+            shapeList.Add(shape);
+            shapes = shapeList.ToArray();
+            if(shape.MaterialAt(new Vector()).Emittance > 0)
             {
-                Console.WriteLine("Material is non-existent for shape:"+shape.ToString());
-                this.ShapeList.Add(shape);
-            } else if (shape.MaterialAt(new Vector()).Emittance > 0)
-            {
-                this.ShapeList.Add(shape);
-                this.Lights = ShapeList.ToArray();
+                lightList.Add(shape);
+                lights = lightList.ToArray();
             }
-            
         }
 
         int RayCount()
         {
-            return this.rays;
+            return rays;
         }
         
-        public Hit Intersect(Ray r)
+        internal Hit Intersect(Ray r)
         {
-            this.rays++;
-            return this.Tree.Intersect(r);
+            Interlocked.Increment(ref rays);
+            return tree.Intersect(r);
         }
     }
 }
