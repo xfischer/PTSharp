887edcd7322abd5b7a5153ba8cf9f99e7e23e229
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-01-30 22:14:30 +0200

    Update Vector.cs

--- Vector.cs
+++ Vector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace PTSharp
 {
-    public class Vector
+    struct Vector
     {
         public static Vector ORIGIN = new Vector(0, 0, 0);
-
         public double X, Y, Z;
-
-        public Vector() {}
 
         public Vector(double x, double y, double z)
         {
@@ -20,14 +17,13 @@ namespace PTSharp
             Y = y;
             Z = z;
         }
-        
         public static Vector RandomUnitVector(Random rnd)
         {
             for (;;)
             {
                 double x, y, z;
 
-                if (rnd == null)
+                if (rnd.Equals(null))
                 {
                     x = new Random().NextDouble() * 2 - 1;
                     y = new Random().NextDouble() * 2 - 1;
@@ -46,113 +42,76 @@ namespace PTSharp
                 return new Vector(x, y, z).Normalize();
             }
         }
-        
-        public double Length()
-        {
-            return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
-        }
-        
+
+        public double Length() => Math.Sqrt(X * X + Y * Y + Z * Z);
+
         public double LengthN(double n)
         {
             if (n == 2)
             {
-                return this.Length();
+                return Length();
             }
-            Vector a = this.Abs();
+            var a = Abs();
             return Math.Pow(Math.Pow(a.X, n) + Math.Pow(a.Y, n) + Math.Pow(a.Z, n), 1 / n);
         }
-        
+
         public double Dot(Vector b)
         {
-            return this.X * b.X + this.Y * b.Y + this.Z * b.Z;
+            return X * b.X + Y * b.Y + Z * b.Z;
         }
 
         public Vector Cross(Vector b)
         {
-            return new Vector(this.Y * b.Z - this.Z * b.Y,  
-                              this.Z * b.X - this.X * b.Z, 
-                              this.X * b.Y - this.Y * b.X);
+            return new Vector(Y * b.Z - Z * b.Y,
+                              Z * b.X - X * b.Z,
+                              X * b.Y - Y * b.X);
         }
 
         public Vector Normalize()
         {
-            double d = this.Length();
-            return new Vector(this.X / d, this.Y / d, this.Z / d);
+            var d = Length();
+            return new Vector(X / d, Y / d, Z / d);
         }
 
         public Vector Negate()
         {
-            return new Vector(-this.X, -this.Y, -this.Z);
+            return new Vector(-X, -Y, -Z);
         }
 
         Vector Abs()
         {
-            return new Vector(Math.Abs(this.X), Math.Abs(this.Y), Math.Abs(this.Z));
+            return new Vector(Math.Abs(X), Math.Abs(Y), Math.Abs(Z));
         }
 
-        public Vector Add(Vector b)
-        {
-            return new Vector(this.X + b.X, this.Y + b.Y, this.Z + b.Z);
-        }
+        public Vector Add(Vector b) => new Vector(X + b.X, Y + b.Y, Z + b.Z);
 
-        public Vector Sub(Vector b)
-        {
-            return new Vector(this.X - b.X, this.Y - b.Y, this.Z - b.Z);
-        }
+        public Vector Sub(Vector b) => new Vector(X - b.X, Y - b.Y, Z - b.Z);
 
-        public Vector Mul(Vector b)
-        {
-            return new Vector(this.X * b.X, this.Y * b.Y, this.Z * b.Z);
-        }
+        public Vector Mul(Vector b) => new Vector(X * b.X, Y * b.Y, Z * b.Z);
 
-        public Vector Div(Vector b)
-        {
-            return new Vector(this.X / b.X, this.Y / b.Y, this.Z / b.Z);
-        }
+        public Vector Div(Vector b) => new Vector(X / b.X, Y / b.Y, Z / b.Z);
 
-        public Vector Mod(Vector b)
-        {
-            return new Vector(this.X - b.X * Math.Floor(this.X / b.X), 
-                              this.Y - b.Y * Math.Floor(this.Y / b.Y), 
-                              this.Z - b.Z * Math.Floor(this.Z / b.Z));
-        }
-        
-        public Vector AddScalar(double b)
-        {
-            return new Vector(this.X + b, this.Y + b, this.Z + b);
-        }
+        public Vector Mod(Vector b) => new Vector(X - (b.X * Math.Floor(X / b.X)),
+                                                  Y - (b.Y * Math.Floor(Y / b.Y)),
+                                                  Z - (b.Z * Math.Floor(Z / b.Z)));
 
-        public Vector SubScalar(double b)
-        {
-            return new Vector(this.X - b, this.Y - b, this.Z - b);
-        }
+        public Vector AddScalar(double b) => new Vector(X + b, Y + b, Z + b);
 
-        public Vector MulScalar(double b)
-        {
-            return new Vector(this.X * b, this.Y * b, this.Z * b);
-        }
+        public Vector SubScalar(double b) => new Vector(X - b, Y - b, Z - b);
 
-        public Vector DivScalar(double b)
-        {
-            return new Vector(this.X / b, this.Y / b, this.Z / b);
-        }
+        public Vector MulScalar(double b) => new Vector(X * b, Y * b, Z * b);
 
-        public Vector Min(Vector b)
-        {
-            return new Vector(Math.Min(this.X, b.X), Math.Min(this.Y, b.Y), Math.Min(this.Z, b.Z));
-        }
+        public Vector DivScalar(double b) => new Vector(X / b, Y / b, Z / b);
 
-        public Vector Max(Vector b)
-        {
-            return new Vector(Math.Max(this.X, b.X), Math.Max(this.Y, b.Y), Math.Max(this.Z, b.Z));
-        }
+        public Vector Min(Vector b) => new Vector(Math.Min(X, b.X), Math.Min(Y, b.Y), Math.Min(Z, b.Z));
+
+        public Vector Max(Vector b) => new Vector(Math.Max(X, b.X), Math.Max(Y, b.Y), Math.Max(Z, b.Z));
 
         public Vector MinAxis()
         {
-            double x, y, z;
-            x = Math.Abs(this.X);
-            y = Math.Abs(this.Y);
-            z = Math.Abs(this.Z);
+            var x = Math.Abs(this.X);
+            var y = Math.Abs(this.Y);
+            var z = Math.Abs(this.Z);
 
             if (x <= y && x <= z)
             {
@@ -165,32 +124,23 @@ namespace PTSharp
             return new Vector(0, 0, 1);
         }
 
-        public double MinComponent()
-        {
-            return Math.Min(Math.Min(this.X, this.Y), this.Z);
-        }
+        public double MinComponent() => Math.Min(Math.Min(X, Y), Z);
 
-        public double MaxComponent()
-        {
-            return Math.Max(Math.Max(this.X, this.Y), this.Z);
-        }
+        public double MaxComponent() => Math.Max(Math.Max(X, Y), Z);
 
-        public Vector Reflect(Vector i)
-        {
-            return i.Sub(this.MulScalar(2 * this.Dot(i)));
-        }
+        public Vector Reflect(Vector i) => i.Sub(MulScalar(2 * Dot(i)));
 
         public Vector Refract(Vector i, double n1, double n2)
         {
             double nr = n1 / n2;
-            double cosI = -this.Dot(i);
+            double cosI = -Dot(i);
             double sinT2 = nr * nr * (1 - cosI * cosI);
             if (sinT2 > 1)
             {
                 return new Vector();
             }
             double cosT = Math.Sqrt(1 - sinT2);
-            return i.MulScalar(nr).Add(this.MulScalar(nr * cosI - cosT));
+            return i.MulScalar(nr).Add(MulScalar(nr * cosI - cosT));
         }
 
         public double Reflectance(Vector i, double n1, double n2)
@@ -203,9 +153,9 @@ namespace PTSharp
                 return 1;
             }
             double cosT = Math.Sqrt(1 - sinT2);
-            double rOrth = (n1 * cosI - n2 * cosT) / (n1 * cosI + n2 * cosT);
-            double rPar = (n2 * cosI - n1 * cosT) / (n2 * cosI + n1 * cosT);
-            return (rOrth * rOrth + rPar * rPar) / 2;
+            double rOrth = ((n1 * cosI) - (n2 * cosT)) / ((n1 * cosI) + (n2 * cosT));
+            var rPar = ((n2 * cosI) - (n1 * cosT)) / ((n2 * cosI) + (n1 * cosT));
+            return ((rOrth * rOrth) + (rPar * rPar)) / 2;
         }
-    }
+    };
 }
