a5ad4b6eed73e5e270a2c0262caeae0adb1395e3
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-01-30 23:00:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,22 +10,25 @@ namespace PTSharp
     {
         static void Main(string[] args)
         {
-            int width = 1920;
-            int height = 1080;
+            int width = 1280;
+            int height = 720;
             Scene scene = new Scene();
+            
             scene.Add(Sphere.NewSphere(new Vector(1.5, 1.25, 0), 1.25, Material.SpecularMaterial(Color.HexColor(0x004358), 1.3)));
             scene.Add(Sphere.NewSphere(new Vector(-1, 1, 2), 1, Material.SpecularMaterial(Color.HexColor(0xFFE11A), 1.3)));
             scene.Add(Sphere.NewSphere(new Vector(-2.5, 0.75, 0), 0.75, Material.SpecularMaterial(Color.HexColor(0xFD7400), 1.3)));
             scene.Add(Sphere.NewSphere(new Vector(-0.75, 0.5, -1), 0.5, Material.ClearMaterial(1.5, 0)));
             scene.Add(Cube.NewCube(new Vector(-10, -1, -10), new Vector(10, 0, 10), Material.GlossyMaterial(Color.White, 1.1, Util.Radians(10))));
             scene.Add(Sphere.NewSphere(new Vector(-1.5, 4, 0), 0.5, Material.LightMaterial(Color.White, 30)));
+
             Camera camera = Camera.LookAt(new Vector(0, 2, -5), new Vector(0, 0.25, 3), new Vector(0, 1, 0), 45);
             camera.SetFocus(new Vector(-0.75, 1, -1), 0.1);
-            Sampler sampler = DefaultSampler.NewSampler(4, 8);
+
+            DefaultSampler sampler = DefaultSampler.NewSampler(4, 8);
+            sampler.SMode = SpecularMode.SpecularModeFirst;
+
             Renderer renderer = Renderer.NewRenderer(scene, camera, sampler, width, height);
-            renderer.AdaptiveSamples = 50;
-            renderer.FireflySamples = 32;
-            renderer.IterativeRender("./out.png", 2);           
+            renderer.IterativeRender("./out.png", 100);           
         }
     }
 }
