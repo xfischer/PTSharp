f5f4d32916a3430e85fba0a0b45f8e4b45219732
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-04-06 11:08:32 +0300

    Switch to Tuple<> return

--- Box.cs
+++ Box.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace PTSharp
 {
-    struct Box
+    class Box
     {
         public Vector Min;
         public Vector Max;
+        internal bool left;
+        internal bool right;
+
+        internal Box() { }
 
         public Box(Vector min, Vector max)
         {
@@ -20,28 +24,30 @@ namespace PTSharp
 
         internal static Box BoxForShapes(IShape[] shapes)
         {
-            if (shapes.Length.Equals(0))
+	        if(shapes.Length == 0)
             {
                 return new Box();
             }
-            Box box = shapes[0].GetBoundingBox();
-            foreach (var shape in shapes)
+            var box = shapes[0].BoundingBox();
+
+           foreach(var shape in shapes)
             {
-                box = box.Extend(shape.GetBoundingBox());
-            }
+                box = box.Extend(shape.BoundingBox());
+
+	        }
             return box;
         }
 
-        internal static Box BoxForTriangles(IShape[] shapes)
+        internal static Box BoxForTriangles(Triangle[] shapes)
         {
             if (shapes.Length == 0)
             {
                 return new Box();
             }
-            Box box = shapes[0].GetBoundingBox();
+            Box box = shapes[0].BoundingBox();
             foreach (var shape in shapes)
             {
-                box = box.Extend(shape.GetBoundingBox());
+                box = box.Extend(shape.BoundingBox());
             }
             return box;
         }
@@ -54,18 +60,21 @@ namespace PTSharp
 
         public double InnerRadius() => Center().Sub(Min).MaxComponent();
 
-        public Vector Size() => Max.Sub(this.Min);
+        public Vector Size()
+        {
+            return Max.Sub(Min);
+        }
 
         public Box Extend(Box b) => new Box(Min.Min(b.Min), Max.Max(b.Max));
 
-        public Boolean Contains(Vector b) => Min.X <= b.X && Max.X >= b.X &&
-                   Min.Y <= b.Y && Max.Y >= b.Y &&
-                   Min.Z <= b.Z && Max.Z >= b.Z;
+        public bool Contains(Vector b) => Min.X <= b.X && Max.X >= b.X &&
+                                             Min.Y <= b.Y && Max.Y >= b.Y &&
+                                             Min.Z <= b.Z && Max.Z >= b.Z;
 
-        public Boolean Intersects(Box b) => !(this.Min.X > b.Max.X || this.Max.X < b.Min.X || this.Min.Y > b.Max.Y ||
+        public bool Intersects(Box b) => !(this.Min.X > b.Max.X || this.Max.X < b.Min.X || this.Min.Y > b.Max.Y ||
                      this.Max.Y < b.Min.Y || this.Min.Z > b.Max.Z || this.Max.Z < b.Min.Z);
 
-        public double[] Intersect(Ray r)
+        public (double, double) Intersect(Ray r)
         {
             var x1 = (Min.X - r.Origin.X) / r.Direction.X;
             var y1 = (Min.Y - r.Origin.Y) / r.Direction.Y;
@@ -73,6 +82,7 @@ namespace PTSharp
             var x2 = (Max.X - r.Origin.X) / r.Direction.X;
             var y2 = (Max.Y - r.Origin.Y) / r.Direction.Y;
             var z2 = (Max.Z - r.Origin.Z) / r.Direction.Z;
+
             if (x1 > x2)
             {
                 (x1, x2) = (x2, x1);
@@ -87,39 +97,28 @@ namespace PTSharp
             }
             double t1 = Math.Max(Math.Max(x1, y1), z1);
             double t2 = Math.Min(Math.Min(x2, y2), z2);
-            double[] result = { t1, t2 };
 
-            return result;
+            return (t1, t2);
         }
 
-        public bool[] Partition(Axis axis, double point)
+        public (bool, bool) Partition(Axis axis, double point)
         {
-            bool left;
-            bool right;
-            bool[] result = new bool[2];
-
             switch (axis)
             {
                 case Axis.AxisX:
                     left = Min.X <= point;
                     right = Max.X >= point;
-                    result[0] = left;
-                    result[1] = right;
                     break;
                 case Axis.AxisY:
-                    left = this.Min.Y <= point;
-                    right = this.Max.Y >= point;
-                    result[0] = left;
-                    result[1] = right;
+                    left = Min.Y <= point;
+                    right = Max.Y >= point;
                     break;
                 case Axis.AxisZ:
-                    left = this.Min.Z <= point;
-                    right = this.Max.Z >= point;
-                    result[0] = left;
-                    result[1] = right;
+                    left = Min.Z <= point;
+                    right = Max.Z >= point;
                     break;
             }
-            return result;
+            return (left, right);
         }
-    };
+    }
 }        
