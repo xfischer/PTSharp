e87a9c03d7ed0bdafbcbc1eb781b07ccfa58ad06
Antonios Kavasis <1996380+akav@users.noreply.github.com>
2019-05-18 10:41:16 +0300

    Update SH.cs

--- SH.cs
+++ SH.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PTSharp
 {
@@ -15,44 +11,42 @@ namespace PTSharp
         func harmonicFunction;
         Mesh mesh;
         
-        IShape NewSphericalHarmonic(int l, int m, Material pm, Material nm)
+        internal static IShape NewSphericalHarmonic(int l, int m, Material pm, Material nm)
         {
-            SphericalHarmonic sh = new SphericalHarmonic();
+            var sh = new SphericalHarmonic();
             sh.PositiveMaterial = pm;
             sh.NegativeMaterial = nm;
             sh.harmonicFunction = shFunc(l, m);
-            sh.mesh = new MC().NewSDFMesh(sh, sh.BoundingBox(), 0.01);
+            sh.mesh = MC.NewSDFMesh(sh, sh.BoundingBox(), 0.01);
             return sh;
-        }
-
-        Material IShape.MaterialAt(Vector p)
-        {
-            double h = this.EvaluateHarmonic(p);
-            if (h < 0)
-            {
-                return this.NegativeMaterial;
-            }
-            else
-            {
-                return this.PositiveMaterial;
-            }
-
         }
 
         void IShape.Compile()
         {
-            this.mesh.Compile();
+            mesh.Compile();
         }
 
-        Box BoundingBox()
+        Box IShape.BoundingBox()
         {
             double r = 1;
             return new Box(new Vector(-r, -r, -r), new Vector(r, r, r));
         }
 
-        public Hit Intersect(Ray r)
+        Box SDF.BoundingBox()
         {
-            Hit hit = this.mesh.Intersect(r);
+            double r = 1;
+            return new Box(new Vector(-r, -r, -r), new Vector(r, r, r));
+        }
+
+        internal Box BoundingBox()
+        {
+            const int r = 1;
+            return new Box(new Vector(-r, -r, -r), new Vector(r, r, r));
+        }
+
+        Hit IShape.Intersect(Ray r)
+        {
+            var hit = mesh.Intersect(r);
             if (!hit.Ok())
             {
                 return Hit.NoHit;
@@ -62,37 +56,48 @@ namespace PTSharp
 
         Vector IShape.UV(Vector p)
         {
-            double u = Math.Atan2(p.Z, p.X);
-            double v = Math.Atan2(p.Y, new Vector(p.X, 0, p.Z).Length());
-            u = (1 - (u + Math.PI)) / (2 * Math.PI);
-            v = (v + Math.PI / 2) / Math.PI;
-            return new Vector(u, v, 0);
+            return new Vector();
+        }
+
+        Material IShape.MaterialAt(Vector p)
+        {
+            var h = EvaluateHarmonic(p);
+            if(h < 0)
+            {
+                return NegativeMaterial;
+            }
+            else
+            {
+                return PositiveMaterial;
+            }
         }
         
         Vector IShape.NormalAt(Vector p)
         {
-            double e = 0.0001;
-            double x = p.X;
-            double y = p.Y;
-            double z = p.Z;
+            const double e = 0.0001;
+            (var x, var y, var z) = (p.X, p.Y, p.Z);
 
-            Vector n = new Vector(
-                this.Evaluate(new Vector(x - e, y, z)) - this.EvaluateHarmonic(new Vector(x + e, y, z)),
-                this.Evaluate(new Vector(x, y - e, z)) - this.Evaluate(new Vector(x, y + e, z)),
-                this.Evaluate(new Vector(x, y, z - e)) - this.Evaluate(new Vector(x, y, z + e))
-            );
-
+            var n = new Vector(
+                Evaluate(new Vector(x - e, y, z)) - Evaluate(new Vector(x + e, y, z)),
+                Evaluate(new Vector(x, y - e, z)) - Evaluate(new Vector(x, y + e, z)),
+                Evaluate(new Vector(x, y, z - e)) - Evaluate(new Vector(x, y, z + e)));
+	    
             return n.Normalize();
+        }
+
+        double EvaluateHarmonic(Vector p)
+        {
+            return harmonicFunction(p.Normalize());
         }
 
         double Evaluate(Vector p)
         {
-            return p.Length() - Math.Abs(this.harmonicFunction(p.Normalize()));
+            return p.Length() - Math.Abs(harmonicFunction(p.Normalize()));
         }
-            
-        double EvaluateHarmonic(Vector p)
+
+        double SDF.Evaluate(Vector p)
         {
-            return this.harmonicFunction(p.Normalize());
+            return p.Length() - Math.Abs(harmonicFunction(p.Normalize()));
         }
 
         static double sh00(Vector d)
@@ -100,142 +105,142 @@ namespace PTSharp
             return 0.282095;
         }
 
-        double sh1n1(Vector d)
+        static double sh1n1(Vector d)
         {
             return -0.488603 * d.Y;
         }
 
-        double sh10(Vector d)
+        static double sh10(Vector d)
         {
             return 0.488603 * d.Z;
         }
 
-        double sh1p1(Vector d)
+        static double sh1p1(Vector d)
         {
             return -0.488603 * d.X;
         }
 
-        double sh2n2(Vector d)
+        static double sh2n2(Vector d)
         {
             // 0.5 * sqrt(15/pi) * x * y
             return 1.092548 * d.X * d.Y;
         }
 
-        double sh2n1(Vector d)
+        static double sh2n1(Vector d)
         {
             // -0.5 * sqrt(15/pi) * y * z
             return -1.092548 * d.Y * d.Z;
         }
 
-        double sh20(Vector d)
+        static double sh20(Vector d)
         {
             // 0.25 * sqrt(5/pi) * (-x^2-y^2+2z^2)
             return 0.315392 * (-d.X * d.X - d.Y * d.Y + 2.0 * d.Z * d.Z);
         }
 
-        double sh2p1(Vector d)
+        static double sh2p1(Vector d)
         {
             // -0.5 * sqrt(15/pi) * x * z
             return -1.092548 * d.X * d.Z;
         }
 
-        double sh2p2(Vector d)
+        static double sh2p2(Vector d)
         {
             // 0.25 * sqrt(15/pi) * (x^2 - y^2)
             return 0.546274 * (d.X * d.X - d.Y * d.Y);
         }
 
-        double sh3n3(Vector d)
+        static double sh3n3(Vector d)
         {
             // -0.25 * sqrt(35/(2pi)) * y * (3x^2 - y^2)
             return -0.590044 * d.Y * (3.0 * d.X * d.X - d.Y * d.Y);
         }
 
-        double sh3n2(Vector d)
+        static double sh3n2(Vector d)
         {
             // 0.5 * sqrt(105/pi) * x * y * z
             return 2.890611 * d.X * d.Y * d.Z;
         }
 
-        double sh3n1(Vector d)
+        static double sh3n1(Vector d)
         {
             // -0.25 * sqrt(21/(2pi)) * y * (4z^2-x^2-y^2)
             return -0.457046 * d.Y * (4.0 * d.Z * d.Z - d.X * d.X - d.Y * d.Y);
         }
 
-        double sh30(Vector d)
+        static double sh30(Vector d)
         {
             // 0.25 * sqrt(7/pi) * z * (2z^2 - 3x^2 - 3y^2)
             return 0.373176 * d.Z * (2.0 * d.Z * d.Z - 3.0 * d.X * d.X - 3.0 * d.Y * d.Y);
         }
 
-        double sh3p1(Vector d)
+        static double sh3p1(Vector d)
         {
             // -0.25 * sqrt(21/(2pi)) * x * (4z^2-x^2-y^2)
             return -0.457046 * d.X * (4.0 * d.Z * d.Z - d.X * d.X - d.Y * d.Y);
         }
 
-        double sh3p2(Vector d)
+        static double sh3p2(Vector d)
         {
             // 0.25 * sqrt(105/pi) * z * (x^2 - y^2)
             return 1.445306 * d.Z * (d.X * d.X - d.Y * d.Y);
         }
 
-        double sh3p3(Vector d)
+        static double sh3p3(Vector d)
         {
             // -0.25 * sqrt(35/(2pi)) * x * (x^2-3y^2)
             return -0.590044 * d.X * (d.X * d.X - 3.0 * d.Y * d.Y);
         }
-        double sh4n4(Vector d)
+        static double sh4n4(Vector d)
         {
             // 0.75 * sqrt(35/pi) * x * y * (x^2-y^2)
             return 2.503343 * d.X * d.Y * (d.X * d.X - d.Y * d.Y);
         }
 
-        double sh4n3(Vector d)
+        static double sh4n3(Vector d)
         {
             // -0.75 * sqrt(35/(2pi)) * y * z * (3x^2-y^2)
             return -1.770131 * d.Y * d.Z * (3.0 * d.X * d.X - d.Y * d.Y);
         }
 
-        double sh4n2(Vector d)
+        static double sh4n2(Vector d)
         {
             // 0.75 * sqrt(5/pi) * x * y * (7z^2-1)
             return 0.946175 * d.X * d.Y * (7.0 * d.Z * d.Z - 1.0);
         }
 
-        double sh4n1(Vector d)
+        static double sh4n1(Vector d)
         {
             // -0.75 * sqrt(5/(2pi)) * y * z * (7z^2-3)
             return -0.669047 * d.Y * d.Z * (7.0 * d.Z * d.Z - 3.0);
         }
 
-        double sh40(Vector d)
+        static double sh40(Vector d)
         {
             // 3/16 * sqrt(1/pi) * (35z^4-30z^2+3)
             double z2 = d.Z * d.Z;
             return 0.105786 * (35.0 * z2 * z2 - 30.0 * z2 + 3.0);
         }
 
-        double sh4p1(Vector d)
+        static double sh4p1(Vector d)
         {
             // -0.75 * sqrt(5/(2pi)) * x * z * (7z^2-3)
             return -0.669047 * d.X * d.Z * (7.0 * d.Z * d.Z - 3.0);
         }
 
-        double sh4p2(Vector d)
+        static double sh4p2(Vector d)
         {
             // 3/8 * sqrt(5/pi) * (x^2 - y^2) * (7z^2 - 1)
             return 0.473087 * (d.X * d.X - d.Y * d.Y) * (7.0 * d.Z * d.Z - 1.0);
         }
 
-        double sh4p3(Vector d)
+        static double sh4p3(Vector d)
         {
             // -0.75 * sqrt(35/(2pi)) * x * z * (x^2 - 3y^2)
             return -1.770131 * d.X * d.Z * (d.X * d.X - 3.0 * d.Y * d.Y);
         }
 
-        double sh4p4(Vector d)
+        static double sh4p4(Vector d)
         {
             // 3/16*sqrt(35/pi) * (x^2 * (x^2 - 3y^2) - y^2 * (3x^2 - y^2))
             double x2 = d.X * d.X;
@@ -243,7 +248,7 @@ namespace PTSharp
             return 0.625836 * (x2 * (x2 - 3.0 * y2) - y2 * (3.0 * x2 - y2));
         }
         
-        func shFunc(int l, int m)
+        static func shFunc(int l, int m)
         {
             func f = null;
 
@@ -352,16 +357,6 @@ namespace PTSharp
                 Console.WriteLine("unsupported spherical harmonic");
             }
             return f;
-        }
-
-        public Box GetBoundingBox()
-        {
-            throw new NotImplementedException();
-        }
-
-        double SDF.Evaluate(Vector p)
-        {
-            throw new NotImplementedException();
         }
     }
 }
